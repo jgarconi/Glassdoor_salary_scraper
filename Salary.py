0e7096fd5486b93377046eeb439e61faef0739cd
Juliana Garçoni dos Santos <js.garconi@gmail.com>
2024-07-13 20:44:00 -0300

    Add .gitignore and rm unused files

--- Salary.py
+++
@@ -1,7 +0,0 @@
-class Salary:
-    def __init__(self, jobTitle, company, meanPay, city):
-        self.jobTitle = jobTitle
-        self.company = company
-        self.meanPay = meanPay
-        self.city = city
-    #enddef
