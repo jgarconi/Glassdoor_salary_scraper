af55bc5daf02367c8e62562982e6beb1db1718a7
Juliana Garçoni dos Santos <js.garconi@gmail.com>
2024-07-13 19:36:49 -0300

    Add scraper first adapted version

--- scraper.py
+++ scraper.py
@@ -1,135 +1,189 @@
 import time
 import json
-import pickle
 import Salary
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
-username = "" # your email here
-password = "" # your password here
+username = # your email here
+password = # your password here
+
+# Manual options for the company, num pages to scrape, and URL
+pages = 1
+companyName = "globo"
+companyURL = "https://www.glassdoor.com.br/Salário/Globo-Salários-E321393.htm"
 
 def obj_dict(obj):
     return obj.__dict__
-#enddef
 
-def json_export(data, cityName):
-    jsonFile = open("Data/" + cityName + ".json", "w")
-    jsonFile.write(json.dumps(data, indent=4, separators=(',', ': '), default=obj_dict))
-    jsonFile.close()
-#enddef
+def json_export(data):
+	jsonFile = open(companyName + ".json", "w")
+	jsonFile.write(json.dumps(data, indent=4, separators=(',', ': '), default=obj_dict))
+	jsonFile.close()
 
 def init_driver():
-    driver = webdriver.Chrome(executable_path = "./chromedriver")
+    service = Service(executable_path = "/usr/lib/chromium-browser/chromedriver")
+    driver = webdriver.Chrome(service=service)
     driver.wait = WebDriverWait(driver, 10)
     return driver
-#enddef
 
 def login(driver, username, password):
     driver.get("http://www.glassdoor.com/profile/login_input.htm")
     try:
-        user_field = driver.wait.until(EC.presence_of_element_located(
-            (By.NAME, "username")))
-        pw_field = driver.find_element_by_class_name("signin-password")
-        login_button = driver.find_element_by_id("signInBtn")
-        user_field.send_keys(username)
-        user_field.send_keys(Keys.TAB)
-        time.sleep(1)
-        pw_field.send_keys(password)
-        time.sleep(1)
+        element = driver.wait.until(EC.presence_of_element_located(
+            (By.ID, "inlineUserEmail")))
+        time.sleep(2)
+            
+        user_input = driver.find_element(By.ID, "inlineUserEmail")    
+        user_input.send_keys(username)
+        time.sleep(2)
+        
+        continue_button = driver.find_element(By.CLASS_NAME, "emailButton")
+        continue_button.click()
+        time.sleep(2)
+        
+        pw_input = driver.find_element(By.ID, "inlineUserPassword")
+        pw_input.send_keys(password)
+        time.sleep(2)
+
+        login_button = driver.find_element(By.CLASS_NAME, "emailButton")
         login_button.click()
+        time.sleep(2)
+        
+        user_input = driver.find_element(By.ID, "inlineUserEmail")    
+        user_input.send_keys(username)
+        time.sleep(2)
+        
+        continue_button = driver.find_element(By.CLASS_NAME, "emailButton")
+        continue_button.click()
+        time.sleep(2)
+        
+        pw_input = driver.find_element(By.ID, "inlineUserPassword")
+        pw_input.send_keys(password)
+        time.sleep(2)
+        
+        login_button = driver.find_element(By.CLASS_NAME, "emailButton")
+        login_button.click()
+        time.sleep(2)
+        
     except TimeoutException:
         print("TimeoutException! Username/password field or login button not found on glassdoor.com")
-#enddef
-
-def search(driver, city, title):
-    driver.get("https://www.glassdoor.com/Salaries/index.htm")
+        
+def get_period():
     try:
-        search_btn = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-            (By.CLASS_NAME, "showHH")))
-        time.sleep(5)
-        title_field = driver.find_element_by_id("KeywordSearch")
-        city_field = driver.find_element_by_id("LocationSearch")
-        title_field.send_keys(title)
-        title_field.send_keys(Keys.TAB)
+        dp_button = driver.find_element(By.CLASS_NAME, "filter-chip_FilterChip__kAbKx filter-chip_FilterChipSelected__uNi3m")
+        dp_button.click()
         time.sleep(1)
-        city_field.send_keys(city)
-        city_field.send_keys(Keys.RETURN)
+        
+        period_button = driver.find_element(By.ID, "data-selected-value-Pagamento mensal")
+        period_button.click()
         time.sleep(1)
-    except TimeoutException:
-        print("TimeoutException! city/title field or search button not found on glassdoor.com")
+    except NoSuchElementException:
+        print("Cannot choose payment period.")
+
+def parse_reviews_HTML(reviews, data):
+	for review in reviews:
+		length = "-"
+		gotOffer = "-"
+		experience = "-"
+		difficulty = "-"
+		date = review.find("time", { "class" : "date" }).getText().strip()
+		role = review.find("span", { "class" : "reviewer"}).getText().strip()
+		outcomes = review.find_all("div", { "class" : ["tightLt", "col"] })
+		if (len(outcomes) > 0):
+			gotOffer = outcomes[0].find("span", { "class" : "middle"}).getText().strip()
+		#endif
+		if (len(outcomes) > 1):
+			experience = outcomes[1].find("span", { "class" : "middle"}).getText().strip()
+		#endif
+		if (len(outcomes) > 2):
+			difficulty = outcomes[2].find("span", { "class" : "middle"}).getText().strip()
+		#endif
+		appDetails = review.find("p", { "class" : "applicationDetails"})
+		if (appDetails):
+			appDetails = appDetails.getText().strip()
+			tookFormat = appDetails.find("took ")
+			if (tookFormat >= 0):
+				start = appDetails.find("took ") + 5
+				length = appDetails[start :].split('.', 1)[0]
+			#endif
+		else:
+			appDetails = "-"
+		#endif
+		details = review.find("p", { "class" : "interviewDetails"})
+		if (details):
+			s = details.find("span", { "class" : ["link", "moreLink"] })
+			if (s):
+				s.extract() # Remove the "Show More" text and link if it exists
+			#endif
+			details = details.getText().strip()
+		#endif
+		questions = []
+		qs = review.find_all("span", { "class" : "interviewQuestion"})
+		if (qs):
+			for q in qs:
+				s = q.find("span", { "class" : ["link", "moreLink"] })
+				if (s):
+					s.extract() # Remove the "Show More" text and link if it exists
+				#endif
+				questions.append(q.getText().encode('utf-8').strip())
+			#endfor
+		#endif
+		r = Review.Review(date, role, gotOffer, experience, difficulty, length, details, questions)
+		data.append(r)
+	#endfor
+	return data
 #enddef
 
-def parse_salaries_HTML(salaries, data, city):
-    for salary in salaries:
-        jobTitle = "-"
-        company = "-"
-        meanPay = "-"
-        jobTitle = salary.find("a", { "class" : "jobTitle"}).getText().strip()
-        company = salary.find("div", { "class" : "i-emp"}).getText().strip()
-        try:
-            meanPay = salary.find("div", { "class" : "meanPay"}).find("strong").getText().strip()
-        except:
-            meanPay = 'xxx'
-        r = Salary.Salary(jobTitle, company, meanPay, city)
-        data.append(r)
-    #endfor
-    return data
-#enddef
-
-def get_data(driver, URL, city, data, refresh, startPage=1):
-    print "\nPage " + str(startPage)
-    if (refresh):
-        driver.get(URL)
-        print "Getting " + URL
-        time.sleep(2)
-    try:
-        next_btn = driver.find_element_by_class_name("next")
-        next_link = next_btn.find_element_by_css_selector("a").get_attribute('href')
-    except:
-        next_btn = False
-        next_link = False
-    #endif
-    time.sleep(2)
-    HTML = driver.page_source
-    soup = BeautifulSoup(HTML, "html.parser")
-    try:
-        salaries = soup.find("div", { "class" : ["salaryChartModule"] }).find_all("div", { "class" : ["salaryRow"] })
-    except:
-        salaries = False
-    if (salaries):
-        data = parse_salaries_HTML(salaries, data, city)
-        print "Page " + str(startPage) + " scraped."
-        if (next_link):
-            get_data(driver, next_link, city, data, True, startPage + 1)
-    else:
-        print "No data available for", city
-    #endif
-    return data
+def get_data(driver, URL, startPage, endPage, data, refresh):
+	if (startPage > endPage):
+		return data
+	#endif
+	print("\nPage " + str(startPage) + " of " + str(endPage))
+	currentURL = URL + "_IP" + str(startPage) + ".htm"
+	time.sleep(2)
+	#endif
+	if (refresh):
+		driver.get(currentURL)
+		print("Getting " + currentURL)
+	#endif
+	time.sleep(2)
+	HTML = driver.page_source
+	soup = BeautifulSoup(HTML, "html.parser")
+	reviews = soup.find_all("li", { "class" : ["empReview", "padVert"] })
+	if (reviews):
+		data = parse_reviews_HTML(reviews, data)
+		print("Page " + str(startPage) + " scraped.")
+		if (startPage % 10 == 0):
+			print("\nTaking a breather for a few seconds ...")
+			time.sleep(10)
+		#endif
+		get_data(driver, URL, startPage + 1, endPage, data, True)
+	else:
+		print("Waiting ... page still loading or CAPTCHA input required")
+		time.sleep(3)
+		get_data(driver, URL, startPage, endPage, data, False)
+	#endif
+	return data
 #enddef
 
 if __name__ == "__main__":
-    driver = init_driver()
-    time.sleep(3)
-    print "Logging into Glassdoor account ..."
-    login(driver, username, password)
-    time.sleep(10)
-    # search(driver, city, title)
-    print "\nStarting data scraping ..."
-    city_list = open("cities.txt").read().splitlines()
-    data_out = []
-    for city in city_list:
-        search(driver, city, 'Data Scientist')
-        appendable = get_data(driver, driver.current_url, city, [], False, 1)
-        print "\nExporting data to " + city + ".json"
-        if appendable:
-            data_out.append(appendable)
-            json_export(appendable, city)
-    if data_out:
-        json_export(data_out, 'allcities')
-    driver.quit()
+	driver = init_driver()
+	time.sleep(3)
+	print("Logging into Glassdoor account ...")
+	login(driver, username, password)
+	time.sleep(5)
+	print("Choosing payment period ...")
+	get_period()
+	time.sleep(3)
+	print("\nStarting data scraping ...")
+	data = get_data(driver, companyURL[:-4], 1, pages, [], True)
+	print("\nExporting data to " + companyName + ".json")
+	json_export(data)
+	driver.quit()
 #endif
